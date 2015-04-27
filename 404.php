15d094ab4ebe1169e15016d816f5aa068337c86f
sweetyams <willem@mookieyams.com>
2015-04-26 21:13:59 -0400

    404 page

--- 404.php
+++ 404.php
@@ -1,26 +1,66 @@
-<?php get_header(); ?>
+<?php
 
-<!-- Row for main content area -->
-	<div class="small-12 large-8 columns" id="content" role="main">
-	
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php _e('File Not Found', 'reverie'); ?></h1>
-			</header>
-			<div class="entry-content">
-				<div class="error">
-					<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'reverie'); ?></p>
-				</div>
-				<p><?php _e('Please try the following:', 'reverie'); ?></p>
-				<ul> 
-					<li><?php _e('Check your spelling', 'reverie'); ?></li>
-					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'reverie'), home_url()); ?></li>
-					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'reverie'); ?></li>
-				</ul>
-			</div>
-		</article>
+get_header("home"); 
+$url = get_template_directory_uri();
+?>
 
-	</div>
-	<?php get_sidebar(); ?>
-		
-<?php get_footer(); ?>
+<div class="full-width menu-top gray5">
+  
+  <div class="row" ng-controller="MenuCtrl">
+    <div class="small-centered columns small-12">
+      
+      <ul class="nav">
+           <li class="link"><a href="/client">Clients</a></li>
+           <li class="link"><a href="/client">Therapists</a></li>
+           <li class="link"><a href="/blog">Blog</a></li>
+      </ul>
+
+      <a class="logo center" href="/"><span class="i-mp_icon"></span></a> 
+        
+    
+      <ul class="menu-right"> 
+          <li><a  class="button small joinus button-navy hover-blue text-white" href="http://app.mindplan.me/#/join/start">Join Now</a></li>
+          <li class="link"><a href="http://app.mindplan.me/#/signin">Sign In</a></li>
+        </ul>
+     
+    </div>
+  </div>
+
+</div>
+
+<div class="row bottom-150">
+
+  <div class="columns small-12 top-100">
+    <h1 class="bottom-40 page-title">Error (404)</h1>
+    
+    <div class="page-content">
+      We can't find the page you're looking for. You can <?php printf(__('<a href="%s">head back to home</a>', 'reverie'), home_url()); ?>, or <?php _e('click the <a href="javascript:history.back()">Back</a> button', 'reverie'); ?>
+    </div>
+
+
+  </div>
+</div>
+
+<footer class="full navy">
+  <div class="row top-150 bottom-100">
+    <div class="columns small-6 center">
+      
+      <p class="mail"><a class="logo" href="/"><span class="i-mp_icon"></span></a><a href="mailto:hello@mindplan.me" target="_blank">hello@mindplan.me</a></p>
+      <p class="copy top-30">Copyright © 2014 MindPlan Inc. <br>
+All rights reserved.  Canada patents pending MindScan <br>
+is a Canada registered trademark of MindPlan Inc.</p>
+    </div>
+    <div class="columns small-3">
+      <?php dynamic_sidebar("Footer Left"); ?>
+    </div>
+    <div class="columns small-3">
+      <?php dynamic_sidebar("Footer Right"); ?>
+    </div>
+
+  </div>
+</footer>
+
+<?php 
+get_footer("home"); 
+?>
+ 
